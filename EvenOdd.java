0e4a73bcc130536328bb6345549cccf9b01b601e
sayali0212 <113876152+sayali0212@users.noreply.github.com>
2022-09-21 12:06:50 +0530

    Add files via upload

---
+++ EvenOdd.java
@@ -0,0 +1,21 @@
+package Day1;
+// wa java program to check if a number is even or odd 
+public class EvenOdd {
+
+	public static void main(String[] args) {
+		
+		int e=12;
+		 //% used to find reminder
+		if (e%2==0 )
+		{
+			System.out.println("number is even");
+			System.out.println("######");
+		}	
+			else
+			{
+				System.out.println(" number is odd");
+			}
+
+	}
+
+}
